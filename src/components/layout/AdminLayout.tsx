e1d4bf0e5c4d27e70c73f1e9897f4c88805d8dc5
nbfsoftedukasi <133831554+nbfsoftedukasi@users.noreply.github.com>
2025-12-31 06:43:24 +0700

    update src/components/layout/AdminLayout.tsx

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from "react";
+//import { ReactNode } from "react";
+import type { ReactNode } from "react";
 import { Link } from "@tanstack/react-router";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
